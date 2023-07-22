911ffbaf98cdb781e6498a15c0dfe55d25ed6199
Salomão Faleta <faletas41@gmail.com>
2023-07-21 22:08:29 -0300

    novos exercícios

--- exercicios.py
+++ exercicios.py
@@ -1,11 +1,17 @@
 #AQUI ESTÃO TOFDOS OS EXERCICÍOS DE PYTHON (@CURSOEMVIDEO)
 
-'''
-EXERCICÍO 5
+
+
+#EXERCICÍO 5
 numero = int(input('Digite um número'))
 print('O sucessor de {} é {} o antecessor é {}'.format(numero, numero + 1, numero - 1))
-'''
+
 
 #EXERCICÍO 6
 numero = int(input('Digite um número'))
 print('O dobro de {} é {}, o triplo é {} e a raiz quadrada é {:.2f}'.format(numero, numero * 2, numero * 3, numero ** (1/2)))
+
+#EXERCICÍO 7
+media1 = float(input('Qual a sua média? '))
+media2 = float(input('Qual a sua média? '))
+print('A média entre {} e {} é {}'.format(media1, media2, (media1 + media2) / 2))
