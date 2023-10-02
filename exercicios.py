2818ff3c7e830eb9af7c9e327135573e77b3f3a5
Salomao-Faleta <faletas41@gmail.com>
2023-10-02 00:06:49 -0300

    novo exercicio

--- exercicios.py
+++ exercicios.py
@@ -435,3 +435,28 @@ mostrando na tela uma mensagem:
 #     print('O Segundo é maior'. format(numero2))
 # else:
 #     print('Não existe valor maior, os dois são iguais')
+
+
+'''
+    Exercício Python 39: Faça um programa que leia o ano de nascimento de um jovem e informe, 
+    de acordo com a sua idade, se ele ainda vai se alistar ao serviço militar, 
+    se é a hora exata de se alistar ou se já passou do tempo do alistamento. 
+    Seu programa também deverá mostrar o tempo que falta ou que passou do prazo.
+# '''
+
+# from datetime import date
+# atual = date.today().year
+
+# nascimento = int(input('Ano de nascimento: '))
+# idade = atual - nascimento
+# print('Quem nasceu em {} tem {} anos em {}'.format(nascimento, idade, atual ))
+
+
+# if idade == 18:
+#     print('Deve se alistar IMEDIATAMENTE!!!')
+# elif idade < 18:
+#     saldo = 18 - idade
+#     print('Ainda faltam {} anos para seu alistamento'.format(saldo))
+# else:
+#     saldo = idade - 18
+#     print('Você já deveria ter se alistado há {} anos'.format(saldo))
