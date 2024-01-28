375e204bd1c412d81a46a10f76912620fb61daf2
Salomao-Faleta <faletas41@gmail.com>
2024-01-27 22:36:43 -0300

    novo exercicio
    Finalizando o 2° módulo do curso e falta fazer os exercícios em JS

--- exercicios.py
+++ exercicios.py
@@ -1309,4 +1309,39 @@ C) qual é o nome do produto mais barato.
 
 
 
+'''
+Exercício Python 071: Crie um programa que simule o funcionamento de um caixa eletrônico.
+No início, pergunte ao usuário qual será o valor a ser sacado (número inteiro) 
+e o programa vai informar quantas cédulas de cada valor serão entregues. OBS:
+considere que o caixa possui cédulas de R$50, R$20, R$10 e R$1.
+'''
+
+# print('-=-'*20)
+# print('{:^20}'.format('BANCO CEV'))
+# print('-=-'*20)
+
+# valor = int(input('Quanto você que sacar? '))
+# total = valor
+# ced = 50
+# totCed = 0
+
+# while True:
+#     if total >= ced:
+#         total -= ced
+#         totCed += 1
+#     else:
+#         if totCed > 0:
+#             print(f'Total de {totCed} cédulas de R${ced}')
+            
+#         if ced == 50:
+#             ced = 20
+#         elif ced == 20:
+#             ced = 10
+#         elif ced == 10:
+#             ced =1
+#         totCed = 0
+#         if total == 0:
+#             break
+
+# print('VOLTE SEMPRE!')
 
--- public/exercicios.js
+++ public/exercicios.js
@@ -1,4 +1,4 @@
-// Exercício Python 66: Crie um programa que leia números inteiros pelo teclado. 
+// Exercício JS 66: Crie um programa que leia números inteiros pelo teclado. 
 // O programa só vai parar quando o usuário digitar o valor 999, que é a condição de parada.
 // No final, mostre quantos números foram digitados e qual foi a soma entre elas (desconsiderando o flag).
 
@@ -20,11 +20,9 @@
 
 
 
-// Exercício Python 67: Faça um programa que mostre a tabuada de vários números, um de cada vez, 
+// Exercício JS 67: Faça um programa que mostre a tabuada de vários números, um de cada vez, 
 // para cada valor digitado pelo usuário. 
 // O programa será interrompido quando o número solicitado for negativo.
-
-
 
 
 // while (true) {
@@ -45,7 +43,7 @@
 
 
 
-// Exercício Python 68: Faça um programa que jogue par ou ímpar com o computador. 
+// Exercício JS 68: Faça um programa que jogue par ou ímpar com o computador. 
 // O jogo só será interrompido quando o jogador perder, mostrando o total de vitórias consecutivas que ele 
 // conquistou no final do jogo.
 
@@ -84,7 +82,7 @@
 
 
 
-// Exercício Python 69: Crie um programa que leia a idade e o sexo de várias pessoas.
+// Exercício JS 69: Crie um programa que leia a idade e o sexo de várias pessoas.
 // A cada pessoa cadastrada, o programa deverá perguntar se o usuário quer ou não continuar. No final, mostre:
 
 // A) quantas pessoas tem mais de 18 anos.
@@ -132,7 +130,7 @@
 
 
 
-// Exercício Python 70: Crie um programa que leia o nome e o preço de vários produtos. 
+// Exercício JS 70: Crie um programa que leia o nome e o preço de vários produtos. 
 // O programa deverá perguntar se o usuário vai continuar ou não. No final, mostre:
 
 // A) qual é o total gasto na compra.
